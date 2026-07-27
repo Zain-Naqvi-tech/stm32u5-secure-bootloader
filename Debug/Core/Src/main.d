67eaf6c23760dc8395ed368794a709c9bbd7447a
Zain Naqvi <zainnaqvi.200621@gmail.com>
2026-07-27 19:46:05 -0400

    Initial commit: STM32U5 secure bootloader project setup and GPIO bring-up

---
+++ Debug/Core/Src/main.d
@@ -0,0 +1,15 @@
+Core/Src/main.o: ../Core/Src/main.c \
+ C:/Users/zainn/STM32CubeIDE/workspace_2.2.0/Secure\ Bootloader\ Project\ Zain/Core/Inc/stm32u585xx.h \
+ C:/Users/zainn/STM32CubeIDE/workspace_2.2.0/Secure\ Bootloader\ Project\ Zain/CMSIS/core_cm33.h \
+ C:/Users/zainn/STM32CubeIDE/workspace_2.2.0/Secure\ Bootloader\ Project\ Zain/CMSIS/cmsis_version.h \
+ C:/Users/zainn/STM32CubeIDE/workspace_2.2.0/Secure\ Bootloader\ Project\ Zain/CMSIS/cmsis_compiler.h \
+ C:/Users/zainn/STM32CubeIDE/workspace_2.2.0/Secure\ Bootloader\ Project\ Zain/CMSIS/cmsis_gcc.h \
+ C:/Users/zainn/STM32CubeIDE/workspace_2.2.0/Secure\ Bootloader\ Project\ Zain/CMSIS/mpu_armv8.h \
+ C:/Users/zainn/STM32CubeIDE/workspace_2.2.0/Secure\ Bootloader\ Project\ Zain/Core/Inc/system_stm32u5xx.h
+C:/Users/zainn/STM32CubeIDE/workspace_2.2.0/Secure\ Bootloader\ Project\ Zain/Core/Inc/stm32u585xx.h:
+C:/Users/zainn/STM32CubeIDE/workspace_2.2.0/Secure\ Bootloader\ Project\ Zain/CMSIS/core_cm33.h:
+C:/Users/zainn/STM32CubeIDE/workspace_2.2.0/Secure\ Bootloader\ Project\ Zain/CMSIS/cmsis_version.h:
+C:/Users/zainn/STM32CubeIDE/workspace_2.2.0/Secure\ Bootloader\ Project\ Zain/CMSIS/cmsis_compiler.h:
+C:/Users/zainn/STM32CubeIDE/workspace_2.2.0/Secure\ Bootloader\ Project\ Zain/CMSIS/cmsis_gcc.h:
+C:/Users/zainn/STM32CubeIDE/workspace_2.2.0/Secure\ Bootloader\ Project\ Zain/CMSIS/mpu_armv8.h:
+C:/Users/zainn/STM32CubeIDE/workspace_2.2.0/Secure\ Bootloader\ Project\ Zain/Core/Inc/system_stm32u5xx.h:
